711f873e15bf64d1bcca70ad25dfb52490acb054
UMKC CTCFP User <umkc-ctcfpuser@umkc.edu>
2018-02-10 17:15:59 -0600

    completed game logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,12 +6,19 @@ $(document).ready(function() {
     var defendChar = "";
     var attackHealth = 0;
     var defendHealth = 0;
+    var deadDefenders = [];
+    var gameStatus = "";
+    var defenderIsActive = false;
+    var resetButton = $("<button>");
+        resetButton.attr("id", "resetBtn");
+        resetButton.text("Reset");
 
     var kenobi = {
         name: "Obi-Wan Kenobi",
         health: 120,
         attack: 6,
         counterAttack: 12,
+        isClicked: false,
     }
 
     var skywalker = {
@@ -19,6 +26,7 @@ $(document).ready(function() {
         health: 140,
         attack: 12,
         counterAttack: 4,
+        isClicked: false,
     }
 
     var vader = {
@@ -26,6 +34,7 @@ $(document).ready(function() {
         health: 100,
         attack: 8,
         counterAttack: 24,
+        isClicked: false,
     }
 
     var maul = {
@@ -33,6 +42,7 @@ $(document).ready(function() {
         health: 160,
         attack: 10,
         counterAttack: 8,
+        isClicked: false,
     }
 
     //populate the starting health points
@@ -41,63 +51,57 @@ $(document).ready(function() {
     $("#vader-hp").text(vader.health);
     $("#maul-hp").text(maul.health);
 
-    function selectDefender(defender) {
-        if (defender === "kenobi") {
+    function selectDefender(defender, setAttacker) {
+        if (defender.isClicked) return;
+        defendChar.isClicked = true;
+        $("#p1").text("");
+        $("#p2").text("");
+
+        if (defender === kenobi) {
             $("#kenobi-div").appendTo("#defender").css({
                 "background-color": "#000000",
                 "color": "#ffffff"});
-            $("#kenobi-div").removeAttr("value");
-        } else if (defender === "skywalker") {
+        } else if (defender === skywalker) {
             $("#skywalker-div").appendTo("#defender").css({
                 "background-color": "#000000",
                 "color": "#ffffff"});
-            $("#skywalker-div").removeAttr("value");
-        } else if (defender === "vader") {
+        } else if (defender === vader) {
             $("#vader-div").appendTo("#defender").css({
                 "background-color": "#000000",
                 "color": "#ffffff"});
-            $("#vader-div").removeAttr("value");
-        } else if (defender === "maul") {
+        } else if (defender === maul) {
             $("#maul-div").appendTo("#defender").css({
                 "background-color": "#000000",
                 "color": "#ffffff"});
-            $("#maul-div").removeAttr("value");
         }
     }
 
     function selectAttacker(attacker) {
-        if (attacker === "kenobi") {
+        if (attacker.isClicked) return;
+        attackChar.isClicked = true;
+        if (attacker === kenobi) {
             $("#kenobi-div").appendTo("#your-char");
             $("#skywalker-div").appendTo("#enemy-attack").css("background-color", "#ff0000");
             $("#vader-div").appendTo("#enemy-attack").css("background-color", "#ff0000");
             $("#maul-div").appendTo("#enemy-attack").css("background-color", "#ff0000");
-            $("#kenobi-div").removeAttr("value");
-        } else if (attacker === "skywalker") {
+        } else if (attacker === skywalker) {
             $("#skywalker-div").appendTo("#your-char");
             $("#kenobi-div").appendTo("#enemy-attack").css("background-color", "#ff0000");
             $("#vader-div").appendTo("#enemy-attack").css("background-color", "#ff0000");
             $("#maul-div").appendTo("#enemy-attack").css("background-color", "#ff0000");
-            $("#skywalker-div").removeAttr("value");
-        } else if (attacker === "vader") {
+        } else if (attacker === vader) {
             $("#vader-div").appendTo("#your-char");
             $("#kenobi-div").appendTo("#enemy-attack").css("background-color", "#ff0000");
             $("#skywalker-div").appendTo("#enemy-attack").css("background-color", "#ff0000");
             $("#maul-div").appendTo("#enemy-attack").css("background-color", "#ff0000");
-            $("#vader-div").removeAttr("value");
-        } else if (attacker === "maul") {
+        } else if (attacker === maul) {
             $("#maul-div").appendTo("#your-char");
             $("#kenobi-div").appendTo("#enemy-attack").css("background-color", "#ff0000");
             $("#skywalker-div").appendTo("#enemy-attack").css("background-color", "#ff0000");
             $("#vader-div").appendTo("#enemy-attack").css("background-color", "#ff0000");
-            $("#maul-div").removeAttr("value");
         }
     }
 
-    //var kenobi = {
-    //    health: 120,
-    //    attack: 6,
-    //    counterAttack: 12,
-    //}
     function attackHealthCalc(attHealth) {
         attHealth = attHealth - defendChar.counterAttack;
         return attHealth;
@@ -108,13 +112,33 @@ $(document).ready(function() {
         return defHealth;
     }
     
-    function attackDefend(attackObj, aHealth, defendObj, dHealth, aPower) {
-        console.log("dHealth: " + dHealth);
-        console.log("aHealth: " + aHealth);
+    //determines what to do based on characters health and calls the function to write the html messages.
+    function attackDefend(attackObj, defendObj, aPower) {
 
-        if (dHealth <= 0) {
+        //calculates the defenders health after attack
+        defendHealth = defenderHealthCalc(defendHealth, attackPower);
+
+        //calculates the attackers health after attack
+        if (defendHealth > 0) {
+            attackHealth = attackHealthCalc(attackHealth);
+        }
+
+        if (defendObj === kenobi) {
+            $("#kenobi-hp").text(defendHealth);
+        } else if (defendObj === skywalker){
+            $("#skywalker-hp").text(defendHealth);
+        } else if (defendObj === vader){
+            $("#vader-hp").text(defendHealth);
+        } else if (defendObj === maul){
+            $("#maul-hp").text(defendHealth);
+        }
+
+        if (defendHealth <= 0) {
             //new defender logic
-            var gameStatus = "defenderDead";
+            gameStatus = "defenderDead";
+            defenderIsActive = false;
+            deadDefenders.push(defendObj);
+
             if (defendObj === kenobi) {
                 $("#kenobi-div").hide();
             } else if (defendObj === skywalker){
@@ -124,45 +148,111 @@ $(document).ready(function() {
             } else if (defendObj === maul){
                 $("#maul-div").hide();
             }
-            //writeHtml(gameStatus);
 
-        } else if (aHealth <= 0) {
+        } else if (attackHealth <= 0) {
             //game over logic
-            var gameStatus = "gameover";
-            //writeHtml(gameStatus);
+            gameStatus = "gameover";
 
         } else {
             //continue attack logic
-            var gameStatus = "continue";
-            //writeHtml(gameStatus);
+            gameStatus = "continue";
 
         }
+        if (deadDefenders.length >= 3) {
+            gameStatus = "alldefendersDead"
+        }
+
+        if (attackObj === kenobi) {
+            $("#kenobi-hp").text(attackHealth);
+        } else if (attackObj === skywalker){
+            $("#skywalker-hp").text(attackHealth);
+        } else if (attackObj === vader){
+            $("#vader-hp").text(attackHealth);
+        } else if (attackObj === maul){
+            $("#maul-hp").text(attackHealth);
+        }
+
         writeHtml(gameStatus, defendObj, aPower);
     }
 
+    //write html messages based on attack results
     function writeHtml(gameOptions, dObject, attackPwr,) {
-        var resetButton = $("<button>");
-        resetButton.attr("id", "resetBtn");
-        resetButton.text("Reset");
-        
+
         if (gameOptions === "gameover") {
-            $("#p1").text("You have been defeated... GAME OVER!!");
+            $("#p1").text("You have been defeated... GAME OVER!!").css({
+                "color": "#ff0000",
+                "font-size": "25px"
+            });
             $("#p2").text("");
-            $("#defender").append(resetButton);
+            $("#text-display").show().append(resetButton);
+
+            $("#resetBtn").show().on("click", function() {
+                resetGame();
+            })
+            defenderIsActive = true;
         }else if (gameOptions === "defenderDead") {
-            //need logic to send defenderName variable
             $("#p1").text("You have defeated " + dObject.name + " ,choose another enemy");
             $("#p2").text("");
         }else if (gameOptions === "alldefendersDead") {
-            $("#p1").text("You Won!!! GAME OVER!!");
+            $("#p1").text("You Won!!! GAME OVER!!").css({
+                "color": "#00e600",
+                "font-size": "25px"
+            });
             $("#p2").text("");
-            $("#defender").append(resetButton);
+            $("#text-display").append(resetButton);
+
+            $("#resetBtn").show().on("click", function() {
+                resetGame();
+            })
         }else if (gameOptions === "continue") {
-            //need logic for all 3 variables
             $("#p1").text("You attacked " + dObject.name + " for " + attackPwr + " damage."); 
             $("#p2").text(dObject.name + " attacked you back for " + dObject.counterAttack + " damage.");
 
         }
+
+    }
+
+    function resetGame () {
+        clickCount = 0;
+        charClicked = "";
+        attackChar = "";
+        defendChar = "";
+        attackHealth = 0;
+        defendHealth = 0;
+        deadDefenders = [];
+        gameStatus = "";
+        defenderIsActive = false;
+
+        kenobi.isClicked = false;
+        skywalker.isClicked = false;
+        vader.isClicked = false;
+        maul.isClicked = false;
+
+        $("#kenobi-div").show().appendTo("#top-selection").css({
+            "background-color": "#ffffff",
+            "color": "#000000"});
+        $("#skywalker-div").show().appendTo("#top-selection").css({
+            "background-color": "#ffffff",
+            "color": "#000000"});
+        $("#vader-div").show().appendTo("#top-selection").css({
+            "background-color": "#ffffff",
+            "color": "#000000"});
+        $("#maul-div").show().appendTo("#top-selection").css({
+            "background-color": "#ffffff",
+            "color": "#000000"});
+
+        $("#kenobi-hp").text(kenobi.health);
+        $("#skywalker-hp").text(skywalker.health);
+        $("#vader-hp").text(vader.health);
+        $("#maul-hp").text(maul.health);
+
+        $("#p1").text("");
+        $("#p1").text("").css({
+            "color": "#ffffff",
+            "font-size": "100%"
+        });
+        $("#p2").text("");
+        $("#resetBtn").hide();
 
     }
 
@@ -180,14 +270,15 @@ $(document).ready(function() {
             else if (charClicked === "maul") {
                 attackChar = maul;
             }
-        //attackChar = charClicked;
-        selectAttacker(charClicked);
+        
+        selectAttacker(attackChar);
         clickCount++;
-        //attackHealth = attackChar.health - defendChar.counterAttack;
         attackHealth = attackChar.health;
         attackPower = attackChar.attack;
     } else if (clickCount > 0) {          //remaining clicks select the defender
-            charClicked = ($(this).attr("value"));
+        
+        if (defenderIsActive) return;  
+        charClicked = ($(this).attr("value"));
             if (charClicked === "kenobi") {
                 defendChar = kenobi;
             } else if (charClicked === "skywalker") {
@@ -198,22 +289,21 @@ $(document).ready(function() {
             else if (charClicked === "maul") {
                 defendChar = maul;
             }
-            //defendChar = charClicked;
-            selectDefender(charClicked);
+
+            if(defendChar === attackChar) return;
+            defenderIsActive = true;  
+            selectDefender(defendChar, attackChar);
             defendHealth = defendChar.health;
     }
     })
 
     $("#attack-btn").on("click", function() {
-        console.log("attacker: " + attackChar);
-        console.log("defender: " + defendChar);
-        
-        attackHealth = attackHealthCalc(attackHealth);
-        defendHealth = defenderHealthCalc(defendHealth, attackPower);
-        console.log("attack health: " + attackHealth);
-        console.log("defender health: " + defendHealth);
-        attackDefend(attackChar, attackHealth, defendChar, defendHealth, attackPower);
+        if (gameStatus === "gameover") return;
+        if (!defenderIsActive) return;
+        attackDefend(attackChar, defendChar,attackPower);
         attackPower += attackChar.attack;
     })    
+
+    
 
 });
